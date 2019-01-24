ceb4b0d226e0cf361916c79ad76b86713867fb49
Harsh Pandey <Harsh.pandey77@gmail.com>
2019-01-25 01:04:26 +0530

    woah

--- samplepython.py
+++ samplepython.py
@@ -1,8 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.graph_objs as go
+import pandas as pdimport plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
